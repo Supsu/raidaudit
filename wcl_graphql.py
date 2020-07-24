62b6dd35dcec9b1ed11a4be10c0e2db1fc624082
Teemu Laakso <teemulaakso91@gmail.com>
2020-07-24 15:37:39 +0300

    Started backend for getting attendance from WCL GraphQL API. Added wcl_graphql.py to handle those operations and implemented getting an access token and querying guild tags and attendance

---
+++ wcl_graphql.py
@@ -0,0 +1,100 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+class WCLG:
+
+    def __init__(self):
+        load_dotenv()
+        self.region = os.getenv("WOWREGION")
+        self.realm = os.getenv("WOWREALM")
+        self.guild = os.getenv("WOWGUILD")
+        self.api_id = os.getenv("WCL2ID")
+        self.api_secret = os.getenv("WCL2SECRET")
+        self.clientapi = "https://www.warcraftlogs.com/"
+
+    def getToken(self):
+        tokenurl = self.clientapi + "oauth/token"
+        r = requests.post(tokenurl, auth=(self.api_id, self.api_secret), data={
+            "grant_type": "client_credentials"
+            })
+
+        self.token = r.json()["access_token"]
+        print(self.token)
+
+    def getGuildTags(self):
+
+        self.getToken()
+
+        querystring = f"""
+            {{
+                guildData {{
+                    guild ( name: "{self.guild}", serverRegion: "{self.region}", serverSlug: "{self.realm}" ) {{
+                        tags {{
+                            id
+                            name
+                        }}
+                    }}
+                }}
+            }}
+        """
+
+        headers = {"Authorization": "Bearer {0}".format(self.token)}
+
+        r = requests.post(self.clientapi + "api/v2/client", json = {'query': querystring}, headers=headers)
+        
+        tags = r.json()["data"]["guildData"]["guild"]["tags"]
+
+        return tags
+
+
+    def getAttendance(self, tag=None):
+
+        self.getToken()
+
+        tagstring = ""
+        if tag is not None:
+            tagstring = "(guildTagId: {0}".format(tag)
+
+        querystring = f"""
+            {{
+                guildData {{
+                    guild ( name: "{self.guild}", serverRegion: "{self.region}", serverSlug: "{self.realm}" ) {{
+                        attendance {tagstring} {{
+                            data  {{
+                                code
+                                players {{
+                                    name
+                                    type
+                                    presence
+                                }}
+                                startTime
+                                zone {{
+                                    id
+                                    name
+                                }}
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        """
+
+        print(querystring)
+
+        headers = {"Authorization": "Bearer {0}".format(self.token)}
+
+        r = requests.post(self.clientapi + "api/v2/client", json = {'query': querystring}, headers=headers)
+
+        print(r)
+        print(r.json())
+
+        if "error" in r.json():
+            return []
+
+        return r.json()["data"]["guildData"]["guild"]["attendance"]["data"]
+
+if __name__ == "__main__":
+    w = WCLG()
+    print(w.getAttendance())
+    print(w.getGuildTags())
